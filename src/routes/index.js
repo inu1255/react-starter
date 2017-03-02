929aeec91bf8765fc40a22430045af688e0ac7a3
inu1255 <929909260@qq.com>
2017-03-02 22:11:07 +0800

    xx

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,32 +9,24 @@ import banner from "../assets/banner.jpg"
 
 function Index(props) {
   return (
-    <div className={styles.container}>
-      <Menu menu={[
-        {
-            icon:"cloud",
-            to:"index",
-            title:"网站名"
-        },
-        {
-            title:"注册",
-            to:"register",
-            right:true
-        },
-        {
-            title:"登录",
-            to:"login",
-            right:true
-        }
-    ]} theme="dark" mode="horizontal"></Menu>
+    <div className={ styles.container }>
+      <Menu menu={ [ { icon: "cloud", to: "index", title: "网站名" }, { title: "注册", to: "register", right: true }, { title: "登录", to: "login", right: true } ] } theme="dark" mode="horizontal"></Menu>
       <Carousel autoplay>
-        <div><Img src={banner}></Img></div>
-        <div><Img src={banner}></Img></div>
-        <div><Img src={banner}></Img></div>
-        <div><Img src={banner}></Img></div>
+        <div>
+          <Img src={ banner }></Img>
+        </div>
+        <div>
+          <Img src={ banner }></Img>
+        </div>
+        <div>
+          <Img src={ banner }></Img>
+        </div>
+        <div>
+          <Img src={ banner }></Img>
+        </div>
       </Carousel>
     </div>
-  );
+    );
 }
 
 Index.propTypes = {
